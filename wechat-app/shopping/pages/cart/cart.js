e1f6ef11e19f74cac3d5c4018dae227234aac49e
张裕建1128 <1191099069@qq.com>
2019-10-21 17:06:19 +0800

    购物车2

--- wechat-app/shopping/pages/cart/cart.js
+++ wechat-app/shopping/pages/cart/cart.js
@@ -11,6 +11,42 @@ Page({
     totalPrice:''
   },
 
+  selectList(e){
+    let index=e.currentTarget.dataset.index
+    let selected = `carts[${index}].selected`
+    this.setData({
+      [selected]:!this.data.carts[index].selected
+    })
+    this.getTotalPrice()
+    let carts=this.data.carts
+    for(let i=0;i<carts.length;i++){
+      if(!carts[i].selected==true){
+        this.setData({
+          selectAllStatus:false
+        })
+        return
+      }else{
+        this.setData({
+          selectAllStatus:true
+        })
+      }
+      
+    }
+    
+  },
+  selectAll(){
+    let selectAllStatus =this.data.selectAllStatus
+    selectAllStatus =!selectAllStatus
+    let carts=this.data.carts
+    for(let i=0;i<carts.length;i++){
+      carts[i].selected=selectAllStatus
+    }
+    this.setData({
+      selectAllStatus:selectAllStatus,
+      carts:carts
+    })
+  },
+
   getTotalPrice(){
     let carts=this.data.carts
     let total = 0
@@ -22,6 +58,48 @@ Page({
     this.setData({
       totalPrice:total.toFixed(2)
     })
+  },
+
+  addCount(e){
+    let index = e.currentTarget.dataset.index
+    let carts=this.data.carts
+    let num=carts[index].num
+    num=num+1
+    carts[index].num=num
+    this.setData({
+      carts:carts
+    })
+    this.getTotalPrice()
+  },
+  minusCount(e){
+    let index = e.currentTarget.dataset.index
+    let carts=this.data.carts
+    let num=carts[index].num
+    num=num-1
+    if(num<0){
+      num=0
+    }
+    carts[index].num=num
+    this.setData({
+      carts:carts
+    })
+    this.getTotalPrice()
+  },
+  deleteList(e){
+    let index=e.currentTarget.dataset.index
+    let carts=this.data.carts
+    carts.splice(index,1)
+    this.setData({
+      carts:carts
+    })
+    if(!carts.length){
+      this.setData({
+        hasList:false
+      })
+    }else{
+      this.getTotalPrice()
+    }
+    
   },
   /**
    * 生命周期函数--监听页面加载
@@ -47,7 +125,6 @@ Page({
         carts:[
           { id: 1, title: '新鲜芹菜 半斤', image: '/image/s5.png', num: 4, price: 0.01, selected: true },
           { id: 2, title: '素米 500g', image: '/image/s6.png', num: 1, price: 0.03, selected: true }
-
         ]
       })
       this.getTotalPrice()
