50d6dc843d1310707a00ef2d05e088621526f428
张裕建1128 <1191099069@qq.com>
2019-11-02 12:09:18 +0800

    饿了么2

--- vue/vue-ele-app/src/components/header/header.vue
+++ vue/vue-ele-app/src/components/header/header.vue
@@ -1,19 +1,26 @@
 <template>
     <div class="header">
         <div class="content-wrapper">
+
             <div class="avatar">
-                <img src="./brand@2x.png" alt="">
+                <img :src="seller.avatar" alt="" width="64" height="64" />
             </div>
+
             <div class="content">
                 <div class="title">
                     <span class="brand"></span>
-                    <span class="name">粥品香坊(回龙倌)</span>
+                    <span class="name">{{seller.name}}</span>
                 </div>
-                <div class="description">蜂鸟专送/38分钟送达</div>
-                <div class="support">
-                    <span class="icon special"></span>
-                    <span class="text">在线支付满28减5</span>
+                <div class="description">{{seller.description}}/{{seller.deliveryTime}}分钟送达</div>
+                <div class="support" v-if="seller.supports">
+                    <span class="icon" :class="classMap[seller.supports[0].type]"></span>
+                    <span class="text">{{seller.supports[0].description}}</span>
                 </div>
+            </div>
+
+            <div class="support-count">
+                <span class="count">5个</span>
+                <i class="icon-keyboard_arrow_right"></i>
             </div>
         </div>
     </div>
@@ -21,12 +28,18 @@
 
 <script>
 export default {
-    created(){
-        this.$http.get('http://ustbhuangyi.com/sell/api/seller')
-        .then((res)=>{
-            console.log(res)
-        })
+    props: {
+        seller:{
+            type:Object,
+            default:{}
+        }
+    },
+    data(){
+        return{
+            classMap:['decrease','discount','special','invoice','guarantee']
+        }
     }
+
 }
 </script>
 
@@ -82,14 +95,32 @@ export default {
                         bg-image('decrease_1')
                     &.discount
                         bg-image('discount_1')
-                    &.invoice
-                        bg-image('invoice_1')
                     &.special
                         bg-image('special_1')
+                    &.invoice
+                        bg-image('invoice_1')
                     &.guarantee
                         bg-image('guarantee_1')
                 .text
                     line-height 12px
                     font-size 10px
+        .support-count
+            position absolute
+            right 12px
+            bottom 14px
+            height 24px
+            line-height 24px
+            padding 0 8px
+            font-size 10px
+            background-color rgba(0,0,0,0.2)
+            border-radius 14px
+            text-align center
+            .count
+                vertical-align top
+            .icon-keyboard_arrow_right
+                margin-left 2px
+                line-height 24px
+
+
 
 </style>
