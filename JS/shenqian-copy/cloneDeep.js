5517e7c74d329661cad3fb419b5ca3926745fb0c
张裕建1128 <1191099069@qq.com>
2019-11-08 16:53:20 +0800

    深浅拷贝 栈

--- JS/shenqian-copy/cloneDeep.js
+++ JS/shenqian-copy/cloneDeep.js
@@ -1,3 +1,4 @@
+//浅拷贝
 // function cloneShallow(source){
 //     var target = {}
 //     for (var key in source){
@@ -7,20 +8,21 @@
 //     }
 //     return target
 // }
-
-function cloneDeep(source){
-    var target = {}
-    for (var key in source){
-        if (Object.prototype.hasOwnProperty.call(source, key)){
-            if (typeof source[key] === 'object'){
-                target[key] =cloneDeep(source[key])
-            }else{
-                target[key] = source[key]
-            }
-        }
-    }
-    return target
-}
+//深拷贝
+// function cloneDeep(source){
+//     if(typeof source !== 'object'){return source}
+//     var target = Array.isArray(source)?[]:{}
+//     for (var key in source){
+//         if (Object.prototype.hasOwnProperty.call(source, key)){
+//             if (typeof source[key] === 'object'){
+//                 target[key] =cloneDeep(source[key])
+//             }else{
+//                 target[key] = source[key]
+//             }
+//         }
+//     }
+//     return target
+// }
 let a = {
     name: 'wn',
     book:{
@@ -28,8 +30,50 @@ let a = {
         price: '45'
     }
 }
-b = cloneDeep(a)
+let b = cloneDeep2(a)
 console.log(b)
 a.name = '蜗牛'
 a.book.price = '55'
 
+
+function cloneDeep2(x){
+    const root = {}
+
+    //栈
+    const loopList = [
+        {
+            parent: root,
+            key: undefined,
+            data: x
+        }
+    ]
+
+    while (loopList.length){
+        //广度优先
+        const node = loopList.pop()
+        const parent = node.parent
+        const key = node.key
+        const data =node.data
+
+        let res = parent // {}
+        //初始化赋值目标,key为undefined的话拷贝到父元素，否则拷贝到子元素
+        if (typeof key !== 'undefined'){
+            res = parent[key] = {}
+        }
+        for (let k in data){
+            if(data.hasOwnProperty(k)){
+                if (typeof data[k] ==='object'){
+                    //进行下一次循环
+                    loopList.push({
+                        parent: res,
+                        key: k,
+                        data: data[k]
+                    })
+                }else{
+                    res[k] = data[k]
+                }
+            }
+        }
+    }
+    return root
+}
