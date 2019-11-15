cbc1bd9d5b88986a06f535ccfc73b8d22d98157c
张裕建1128 <1191099069@qq.com>
2019-11-15 21:25:03 +0800

    天气预报

--- vue/vue-communication/src/components/view/demo4/father.vue
+++ vue/vue-communication/src/components/view/demo4/father.vue
@@ -1,34 +1,31 @@
 <template>
-  <div class="container">
-      <h2>父组件</h2>
-      <div class="red">{{xinxiaoxi}}</div>
-    <v-child v-bind:cjieshou="fmsg" v-on:xiaoxi="fjieshou"></v-child>
-  </div>
- 
+    <div class="container">
+        <h2>父组件</h2>
+        <!-- <child1 :title="oTitle"></child1>
+        <child2 @test="getTitle"></child2> -->
+        <child1></child1>
+        <child2></child2>
+    </div>
 </template>
 
 <script>
-import child from './child'
+import child1 from './child1'
+import child2 from './child2'
 export default {
     components: {
-        'v-child':child
+        child1,
+        child2
     },
     data(){
         return{
-            fmsg:'这是父组件的信息',
-            xinxiaoxi:''
         }
     },
     methods:{
-        fjieshou(e){
-            this.xinxiaoxi = e;
-        }
+
     }
 }
 </script>
 
 <style>
-.red{
-    background-color: red;
-}
+
 </style>
