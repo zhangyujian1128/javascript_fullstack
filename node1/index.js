6b9c1e5f7618ede3a48f2c3a0cd24659eed6cabb
张裕建1128 <1191099069@qq.com>
2019-11-29 21:37:02 +0800

    全栈

---
+++ node1/index.js
@@ -0,0 +1,33 @@
+const Koa = require('koa')
+const path = require('path')
+const bodyParser = require('koa-bodyparser')   // 解析表单中间件
+const ejs = require('ejs')      //模板文件
+const session = require('koa-session-minimal')
+const MysqlStore = require('koa-mysql-session')
+const Router=require('koa-router')
+const views = require('koa-views')
+// const koaStatic = require('koa-static')
+const staticCache = require('koa-static-cache')  //静态资源
+const config = require('./config/default.js')
+
+//缓存
+const app = new Koa()
+const router = new Router()
+
+router.get('*',(ctx, next) =>{
+    ctx.body = 'hello world'
+})
+
+app.use(router.routers()  )
+
+app.use(staticCache(path.join(__dirname,'./pubilc'),{ dynamic: true},{ maxAge: 365*24*60*60 }))
+app.use(staticCache(path.join(__dirname,'./images'),{ dynamic: true},{ maxAge: 365*24*60*60 }))
+
+//配置服务器端模板渲染引擎中间件
+app.use(views(path.join(__dirname,'./views'),{
+    extension: 'ejs'
+}))
+
+app.listen(3000)
+console.log(`listening on port ${config.port}`)
+
---
+++ note-book/note_book/controllers/defaultConfig.js
@@ -0,0 +1,11 @@
+const config={
+    database: {
+        DATEBASE: 'test',
+        USERNAME: 'root',
+        PASSWORD: '123456',
+        PORT: '3306',
+        HOST: 'localhost'
+    }
+}
+
+module.exports = config
