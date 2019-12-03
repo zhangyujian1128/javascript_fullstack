4fc1243d64066ec7f293ff54af31f756d0ab27bd
张裕建1128 <1191099069@qq.com>
2019-12-03 17:11:47 +0800

    mixin

---
+++ vue/vue-mixin-drag-loading/src/components/Dialog.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+      <span @mousedown="start" style="position: fixed;top: 0;left: 0">111</span>
+      <el-button @click="open">open</el-button>
+      <el-dialog title="tips" :visible = "visible">
+          <span @click="close">这是一段信息</span>
+      </el-dialog>
+  </div>
+</template>
+
+<script>
+// import
+import dragAbleMix from './drag'
+export default {
+    mixins: [dragAbleMix],
+    components:{
+
+    },
+    data(){
+        return{
+            visible: false
+        }
+    },
+    methods:{
+        open(){
+            this.visible = true
+        },
+        close(){
+            this.visible = false
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
