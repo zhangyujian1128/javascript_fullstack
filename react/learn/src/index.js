a06121ed0627e104853947927d0ccc3424a92235
zhangyujian1128 <1191099069@qq.com>
2020-02-17 16:34:51 +0800

    react初学2

--- react/learn/src/index.js
+++ react/learn/src/index.js
@@ -15,45 +15,143 @@ import * as serviceWorker from './serviceWorker';//pwa支持
 // }
 
 //state
-class Counter extends React.Component {
-    constructor (props) {
+// class Counter extends React.Component {
+//     constructor (props) {
+//         super(props)
+//         // 初始化构造器时设置内部状态num值为1
+//         this.state = {
+//             num: 1
+//         }
+//         this.handleClick = this.handleClick.bind(this)
+//     }
+//     // 生命周期方法，组件渲染完成，只会执行一次
+//     componentDidMount() {
+//         console.log('componentDidMount 函数触发')
+//     }
+//     //生命周期方法，避免组件重复或者无意义的渲染
+//     shouldComponentUpdate(nextProps, nextState){
+//         // console.log(nextProps,nextState)
+//         if(nextState.num%2) {
+//             return true
+//         }
+//         return false
+//     }
+//     handleClick(){
+//         console.log(123)
+//         this.setState({
+//             num: this.state.num + 1
+//         })
+//     }
+//     render (){
+//         return(
+//             <div>
+//                 <p>{this.state.num}</p>
+//                 <button onClick={this.handleClick}>click</button>
+//             </div>  
+//         )
+        
+//     }
+// }
+
+//表单
+//渲染列表
+// class TodoList extends React.Component {
+//     constructor (props) {
+//         super(props)
+//         this.state = {
+//             text: '刷漆的蜗牛',
+//             todos: ['Learn React', 'Learn-Ant-design', 'Learn Koa']
+//         }
+//         this.handleChange = this.handleChange.bind(this)
+//         this.handleClick = this.handleClick.bind(this)
+//     }
+//     handleChange(e){
+//         console.log(e.target.value)
+//         this.setState({
+//             text: e.target.value
+//         })
+//     }
+//     handleClick(){
+//         if(this.state.text){
+//             this.setState({
+//                 //将text值追加到todos数组里面
+//                 todos: [...this.state.todos, this.state.text],
+//                 text: ''
+//             })
+//         }
+//     }
+//     render(){
+//         return(
+//             <div>
+//                 {this.state.text}
+//                 <input type="text" value={this.state.text} onChange={this.handleChange}></input>
+//                 <button onClick={this.handleClick}>add</button>
+//                 <ul>
+//                     {this.state.todos.map( v =>{
+//                     return <li key={v}>{v}</li>
+//                     }) }
+//                 </ul>
+//             </div>
+//         )
+//     }
+// }
+
+// React 16 新增
+// 1. render 函数支持返回数组和字符串
+// 2. 异常处理,添加componentDidCatch钩子 （函数）获取组件错误
+// 3. 新的组件类型
+
+class React16 extends React.Component {
+    constructor(props){
         super(props)
-        // 初始化构造器时设置内部状态num值为1
         this.state = {
-            num: 1
+            hasError: false
+        }
+    }
+    render(){
+        return(
+            <div>
+                {this.state.hasError ? <div>出错了</div> : null}
+                <ClickWriteError></ClickWriteError>
+                <featureReturnFraments></featureReturnFraments>
+            </div>
+        )
+    }
+}
+//ClickWriteError 是一个组件
+class ClickWriteError extends React.Component {
+    constructor (props){
+        super(props)
+        this.state = {
+            error : false
         }
         this.handleClick = this.handleClick.bind(this)
     }
-    // 生命周期方法，组件渲染完成，只会执行一次
-    componentDidMount() {
-        console.log('componentDidMount 函数触发')
-    }
-    //生命周期方法，避免组件重复或者无意义的渲染
-    shouldComponentUpdate(nextProps, nextState){
-        // console.log(nextProps,nextState)
-        if(nextState.num%2) {
-            return true
-        }
-        return false
-    }
     handleClick(){
-        console.log(123)
         this.setState({
-            num: this.state.num + 1
+            error: true
         })
     }
-    render (){
-        return(
-            <div>
-                <p>{this.state.num}</p>
-                <button onClick={this.handleClick}>click</button>
-            </div>  
-        )
-        
+    render(){
+        if(this.state.error){
+            throw new Error('出错了！')
+        }
+        return <button onClick={this.handleClick}>抛出错误</button>
     }
 }
 
-ReactDOM.render(<Counter />, document.getElementById('root'));
+class featureReturnFraments extends React.Component {
+    render() {
+        return [
+            <p key = "key1">React 很不错</p>,
+            '文本1',
+            <p key = "key2">Ant design</p>,
+            '文本2'
+        ]
+    }
+}
+
+ReactDOM.render(<React16 />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
