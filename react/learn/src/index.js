1ca38d2fe258cfdd019d6c8ce79ef2484bc4aa13
zhangyujian1128 <1191099069@qq.com>
2020-02-21 16:49:50 +0800

    React学习6

--- react/learn/src/index.js
+++ react/learn/src/index.js
@@ -1,10 +1,11 @@
 //入口
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Lifecycle from './components/Lifecycle';
-import CommentVs from './components/CommentVs';
-import PureComponentTest from './components/PureComponentTest';
-import Hoc from './components/Hoc';
+// import Lifecycle from './components/Lifecycle';
+// import CommentVs from './components/CommentVs';
+// import PureComponentTest from './components/PureComponentTest';
+import HooksTest from './components/Hookstest'
+import KFormSample from './components/KFormCreate'
 import * as serviceWorker from './serviceWorker';//pwa支持
 
 // class App extends React.Component {
@@ -103,58 +104,60 @@ import * as serviceWorker from './serviceWorker';//pwa支持
 // 2. 异常处理,添加componentDidCatch钩子 （函数）获取组件错误
 // 3. 新的组件类型
 
-class React16 extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            hasError: false
-        }
-    }
-    render(){
-        return(
-            <div>
-                {this.state.hasError ? <div>出错了</div> : null}
-                <ClickWriteError></ClickWriteError>
-                <featureReturnFraments></featureReturnFraments>
-            </div>
-        )
-    }
-}
+// class React16 extends React.Component {
+//     constructor(props){
+//         super(props)
+//         this.state = {
+//             hasError: false
+//         }
+//     }
+//     render(){
+//         return(
+//             <div>
+//                 {this.state.hasError ? <div>出错了</div> : null}
+//                 <ClickWriteError></ClickWriteError>
+//                 <featureReturnFraments></featureReturnFraments>
+//             </div>
+//         )
+//     }
+// }
 //ClickWriteError 是一个组件
-class ClickWriteError extends React.Component {
-    constructor (props){
-        super(props)
-        this.state = {
-            error : false
-        }
-        this.handleClick = this.handleClick.bind(this)
-    }
-    handleClick(){
-        this.setState({
-            error: true
-        })
-    }
-    render(){
-        if(this.state.error){
-            throw new Error('出错了！')
-        }
-        return <button onClick={this.handleClick}>抛出错误</button>
-    }
-}
+// class ClickWriteError extends React.Component {
+//     constructor (props){
+//         super(props)
+//         this.state = {
+//             error : false
+//         }
+//         this.handleClick = this.handleClick.bind(this)
+//     }
+//     handleClick(){
+//         this.setState({
+//             error: true
+//         })
+//     }
+//     render(){
+//         if(this.state.error){
+//             throw new Error('出错了！')
+//         }
+//         return <button onClick={this.handleClick}>抛出错误</button>
+//     }
+// }
 
-class featureReturnFraments extends React.Component {
-    render() {
-        return [
-            <p key = "key1">React 很不错</p>,
-            '文本1',
-            <p key = "key2">Ant design</p>,
-            '文本2'
-        ]
-    }
-}
+// class featureReturnFraments extends React.Component {
+//     render() {
+//         return [
+//             <p key = "key1">React 很不错</p>,
+//             '文本1',
+//             <p key = "key2">Ant design</p>,
+//             '文本2'
+//         ]
+//     }
+// }
 
 
-ReactDOM.render(<Hoc state="React"/>, document.getElementById('root'));
+// ReactDOM.render(<Hoc state="React"/>, document.getElementById('root'));
+ReactDOM.render(<KFormSample/>, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
