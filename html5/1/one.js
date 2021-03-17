bbbd0e95f4e80fc197cb4d9bfa2479c55e044d36
zyj <1191099069@qq.com>
2021-03-17 22:30:31 +0800

    排序方法

--- html5/1/one.js
+++ html5/1/one.js
@@ -395,28 +395,43 @@
 
 
 //冒泡排序法   排序方法1
-var arr1 = [14,23,11,21,34,21]; //由小到大排列
-function sort(arr){
-    // for(let i = 0; i < (arr.length - 1); i++){
-    //     for(let j = 0; j < arr.length-1; j++){
-    //         if(arr[j] > arr[j+1]){
-    //             let temp = arr[j+1];
-    //             arr[j+1] = arr[j];
-    //             arr[j] = temp;
-    //         }
-    //     }
-    // }
-    for(let i = 0; i < arr.length - 1;i++){
-        for(let j = i+1;j < arr.length;j++){
-            if(arr[i]>arr[j]){
-                let temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-    }
-    console.log(arr)
-}
-sort(arr1)
+// var arr1 = [14,23,11,21,34,21]; //由小到大排列
+// function sort(arr){
+//     // for(let i = 0; i < (arr.length - 1); i++){
+//     //     for(let j = 0; j < arr.length-1; j++){
+//     //         if(arr[j] > arr[j+1]){
+//     //             let temp = arr[j+1];
+//     //             arr[j+1] = arr[j];
+//     //             arr[j] = temp;
+//     //         }
+//     //     }
+//     // }
+//     for(let i = 0; i < arr.length - 1;i++){
+//         for(let j = i+1;j < arr.length;j++){
+//             if(arr[i]>arr[j]){
+//                 let temp = arr[i];
+//                 arr[i] = arr[j];
+//                 arr[j] = temp;
+//             }
+//         }
+//     }
+//     console.log(arr)
+// }
+// sort(arr1)
 //show()
+
+// var sort = function(){
+//     console.log(1)
+// }
+// sort();
+
+var sort = function(){
+    setTimeout(()=>{
+        console.log('abc')
+    },3000)
+}
+sort();
+
+
+
 
